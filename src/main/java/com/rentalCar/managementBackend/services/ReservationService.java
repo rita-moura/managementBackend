214634836fa7841918311a5deab36272a941211d
Rita Moura <105252658+rita-moura@users.noreply.github.com>
2023-11-15 18:19:49 -0300

    Fix corrige os serviços

---
+++ src/main/java/com/rentalCar/managementBackend/services/ReservationService.java
@@ -0,0 +1,46 @@
+package com.rentalCar.managementBackend.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rentalCar.managementBackend.entitys.ReservationEntity;
+import com.rentalCar.managementBackend.entitys.VehicleEntity;
+import com.rentalCar.managementBackend.repositorys.ReservationRepository;
+
+
+@Service
+public class ReservationService {
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    public List<ReservationEntity> getAllReservations() {
+        return reservationRepository.findAll();
+    }
+
+     public void insertReservation(Integer vehicleId) {
+        Optional<VehicleEntity> vehicle = vehicleService.getVehicleById(vehicleId);
+        if(vehicle.isPresent()) {
+            ReservationEntity reservation = new ReservationEntity();
+            reservation.setVehicle(vehicle.get());
+            reservationRepository.save(reservation);
+        }
+    }
+
+    public void insertOrChangeReservation(ReservationEntity reservation) {
+        reservationRepository.save(reservation);
+    }
+
+    public void deleteReservationById(Integer id) {
+        reservationRepository.deleteById(id);
+    }
+
+    public Optional<ReservationEntity> getReservationById(Integer id) {
+        return reservationRepository.findById(id);
+    }
+}
