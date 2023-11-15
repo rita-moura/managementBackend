3bbc3cacb8193f3490f6f4e791e3204b7eb4e5ee
Rita Moura <105252658+rita-moura@users.noreply.github.com>
2023-11-15 09:48:14 -0300

    Fix:
    Corrge acesso ao frontend

--- src/main/java/com/rentalCar/managementBackend/controllers/VehicleController.java
+++ src/main/java/com/rentalCar/managementBackend/controllers/VehicleController.java
@@ -1,10 +1,10 @@
 package com.rentalCar.managementBackend.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +19,7 @@ import com.rentalCar.managementBackend.services.VehicleService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.websocket.server.PathParam;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("vehicles")
 public class VehicleController {
